3c3f45a15f5b4022f9f8ea1822999e23bd29a0d2
fre171 <fre171@csiro.au>
2018-03-01 11:22:23 +1100

    Remove out of date comments and files

--- contrib/docker/deployment_scripts/docker_refresh_all.sh
+++ contrib/docker/deployment_scripts/docker_refresh_all.sh
@@ -7,10 +7,6 @@ if [ -z $COMPOSE_FILE ]; then
     return 1
 fi
 
-# This command should do the job, but it doesn't work all the time? Instead will do manual steps
-# docker-compose -f $COMPOSE_FILE up -d --force-recreate
 docker-compose -f $COMPOSE_FILE stop
-#docker rmi $(docker images -q --filter "dangling=true")
 docker-compose -f $COMPOSE_FILE build --no-cache
 docker-compose -f $COMPOSE_FILE up -d
-#docker-compose -f $COMPOSE_FILE start
