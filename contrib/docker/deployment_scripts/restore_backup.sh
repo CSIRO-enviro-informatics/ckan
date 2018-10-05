329a8007167fee473f2e2454e037c31e9873fded
Ben Leighton <Ben.Leighton@csiro.au>
2018-10-05 14:27:41 +1000

    using COMPOSE_PROJECT_NAME as prefix for transient container names

--- contrib/docker/deployment_scripts/restore_backup.sh
+++ contrib/docker/deployment_scripts/restore_backup.sh
@@ -31,15 +31,15 @@ sleep 20s
 # Run the restore container this container is a container with postgres client tools and a restore script that uses native postgres restore mechanisms to restore ckan and datastore backup dumps that are passed in. For more detail see the ../restore/restore.sh file.
 RESTORE_NAME="restore"
 echo "Restore" 
-docker-compose    run --name $COMPOSE_NAME$RESTORE_NAME -e BYPASS_DB_INIT=true -e CONFIRM_RESTORE='Y' -e PGPASSWORD=$DATABASE_PASSWORD -e CKAN_BACKUP_FILE_NAME=$CKAN_BACKUP_FILE_NAME -e DATASTORE_BACKUP_FILE_NAME=$DATASTORE_BACKUP_FILE_NAME restore /restore.sh 
-docker rm -f $COMPOSE_NAME$RESTORE_NAME
+docker-compose    run --name $COMPOSE_PROJECT_NAME$RESTORE_NAME -e BYPASS_DB_INIT=true -e CONFIRM_RESTORE='Y' -e PGPASSWORD=$DATABASE_PASSWORD -e CKAN_BACKUP_FILE_NAME=$CKAN_BACKUP_FILE_NAME -e DATASTORE_BACKUP_FILE_NAME=$DATASTORE_BACKUP_FILE_NAME restore /restore.sh 
+docker rm -f $COMPOSE_PROJECT_NAME$RESTORE_NAME
 # Unclear when we might need to execute this or the side effects of uncessarily executing it but here for documentation - this might be needed for a major ckan upgrade see the ckan documentation
 #docker-compose   run -e BYPASS_DB_INIT=true ckan ckan-paster --plugin=ckan db upgrade -c /etc/ckan/default/ckan.ini
 # Rebuild the search index in solr with the new records 
 SEARCH_NAME="search"
 echo "Updating search index" 
-docker-compose    run --name $COMPOSE_NAME$SEARCH_NAME -e BYPASS_DB_INIT=true ckan ckan-paster --plugin=ckan search-index rebuild --config=/etc/ckan/default/ckan.ini
-docker rm -f $COMPOSE_NAME$SEARCH_NAME
+docker-compose    run --name $COMPOSE_PROJECT_NAME$SEARCH_NAME -e BYPASS_DB_INIT=true ckan ckan-paster --plugin=ckan search-index rebuild --config=/etc/ckan/default/ckan.ini
+docker rm -f $COMPOSE_PROJECT_NAME$SEARCH_NAME
 # Bring backup the ckan and backup containers - data should now be restored  
 docker-compose stop
 docker-compose up -d 
