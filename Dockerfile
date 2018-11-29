e8b8905f16290311d909a45e2f7aa126e421f1ad
Jonathan Yu <jonathan.yu@csiro.au>
2018-11-29 12:35:10 +1100

    adding changes from damc-docker

--- Dockerfile
+++ Dockerfile
@@ -1,61 +1,133 @@
-# See CKAN docs on installation from Docker Compose on usage
-FROM debian:stretch
+# docker build . -t ckan && docker run -d -p 80:5000 --link db:db --link redis:redis --link solr:solr ckan
+
+FROM debian:jessie
 MAINTAINER Open Knowledge
 
-# Install required system packages
-RUN apt-get -q -y update \
-    && DEBIAN_FRONTEND=noninteractive apt-get -q -y upgrade \
-    && apt-get -q -y install \
-        python-dev \
+ENV CKAN_HOME /usr/lib/ckan/default
+ENV CKAN_CONFIG /etc/ckan/default
+ENV CKAN_STORAGE_PATH /var/lib/ckan
+ENV CKAN_SITE_URL http://localhost:5000
+
+# Install required packages
+RUN apt-get -q -y update && apt-get -q -y upgrade && DEBIAN_FRONTEND=noninteractive apt-get -q -y install \
+		python-dev \
         python-pip \
         python-virtualenv \
-        python-wheel \
         libpq-dev \
-        libxml2-dev \
-        libxslt-dev \
-        libgeos-dev \
-        libssl-dev \
-        libffi-dev \
-        postgresql-client \
-        build-essential \
         git-core \
-        vim \
-        wget \
-    && apt-get -q clean \
-    && rm -rf /var/lib/apt/lists/*
+	libffi-dev \
+	&& apt-get -q clean
+#-------------CKAN ext ldap dependencies----------------------------------------------------
 
-# Define environment variables
-ENV CKAN_HOME /usr/lib/ckan
-ENV CKAN_VENV $CKAN_HOME/venv
-ENV CKAN_CONFIG /etc/ckan
-ENV CKAN_STORAGE_PATH=/var/lib/ckan
+RUN apt-get install -y python-dev libldap2-dev libsasl2-dev libssl-dev gcc
+RUN pip install python-ldap
 
-# Build-time variables specified by docker-compose.yml / .env
-ARG CKAN_SITE_URL
 
-# Create ckan user
-RUN useradd -r -u 900 -m -c "ckan account" -d $CKAN_HOME -s /bin/false ckan
+#CKAN spatial ext dependencies
+RUN apt-get install -y libxml2-dev libxslt1-dev libgeos-c1
 
-# Setup virtual environment for CKAN
-RUN mkdir -p $CKAN_VENV $CKAN_CONFIG $CKAN_STORAGE_PATH && \
-    virtualenv $CKAN_VENV && \
-    ln -s $CKAN_VENV/bin/pip /usr/local/bin/ckan-pip &&\
-    ln -s $CKAN_VENV/bin/paster /usr/local/bin/ckan-paster
+# SetUp Virtual Environment CKAN
+RUN mkdir -p $CKAN_HOME $CKAN_CONFIG $CKAN_STORAGE_PATH
+RUN virtualenv $CKAN_HOME
+RUN ln -s $CKAN_HOME/bin/pip /usr/local/bin/ckan-pip
+RUN ln -s $CKAN_HOME/bin/paster /usr/local/bin/ckan-paster
+RUN /bin/bash -c "source $CKAN_HOME/bin/activate && $CKAN_HOME/bin/easy_install-2.7 -U \"pip==9.0.3\" \"setuptools==23.2.1\" && deactivate"
+RUN rm -f $CKAN_HOME/lib/python2.7/site-packages/setuptools-23.2.1-py2.7.egg
 
-# Setup CKAN
-ADD . $CKAN_VENV/src/ckan/
-RUN ckan-pip install -U pip && \
-    ckan-pip install --upgrade --no-cache-dir -r $CKAN_VENV/src/ckan/requirement-setuptools.txt && \
-    ckan-pip install --upgrade --no-cache-dir -r $CKAN_VENV/src/ckan/requirements.txt && \
-    ckan-pip install -e $CKAN_VENV/src/ckan/ && \
-    ln -s $CKAN_VENV/src/ckan/ckan/config/who.ini $CKAN_CONFIG/who.ini && \
-    cp -v $CKAN_VENV/src/ckan/contrib/docker/ckan-entrypoint.sh /ckan-entrypoint.sh && \
-    chmod +x /ckan-entrypoint.sh && \
-    chown -R ckan:ckan $CKAN_HOME $CKAN_VENV $CKAN_CONFIG $CKAN_STORAGE_PATH
+# SetUp Requirements
+RUN mkdir -p $CKAN_HOME/src/ckan
+COPY ./requirements.txt $CKAN_HOME/src/ckan/requirements.txt
+RUN ckan-pip install --upgrade -r $CKAN_HOME/src/ckan/requirements.txt
 
-ENTRYPOINT ["/ckan-entrypoint.sh"]
+COPY ./dev-requirements.txt $CKAN_HOME/src/ckan/dev-requirements.txt
+RUN ckan-pip install --upgrade -r $CKAN_HOME/src/ckan/dev-requirements.txt
 
-USER ckan
+#-------------uwsgi and paste-deploy for prod deployments----------------------------------------------------
+
+RUN pip install uwsgi 
+RUN pip install PasteDeploy 
+
+# TMP-BUGFIX https://github.com/ckan/ckan/issues/3594
+RUN ckan-pip install --upgrade urllib3
+
+# SetUp CKAN
+#ADD . $CKAN_HOME/src/ckan/
+COPY ./ckan $CKAN_HOME/src/ckan/ckan
+COPY ./ckanext $CKAN_HOME/src/ckan/ckanext
+COPY ./contrib $CKAN_HOME/src/ckan/contrib
+COPY ./setup.cfg ./setup.py ./requirement-setuptools.txt ./dev-requirements.txt $CKAN_HOME/src/ckan/
+RUN ckan-pip install -e $CKAN_HOME/src/ckan/
+RUN ln -s $CKAN_HOME/src/ckan/ckan/config/who.ini $CKAN_CONFIG/who.ini
+#COPY ./contrib/docker/config/ckan.ini $CKAN_CONFIG/ckan.ini
+
+# Setup Remote Debugging for Pycharm
+RUN . /usr/lib/ckan/default/bin/activate && pip install pydevd
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/NaturalHistoryMuseum/ckanext-dev.git#egg=ckanext-dev
+
+# Setup LDAP ckan Plugin
+RUN . /usr/lib/ckan/default/bin/activate && pip install "Jinja2==2.9.3"
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/NaturalHistoryMuseum/ckanext-ldap.git@v1.0.1#egg=ckanext-ldap
+RUN . /usr/lib/ckan/default/bin/activate && pip install -r /usr/lib/ckan/default/src/ckanext-ldap/requirements.txt
+
+# setup ckanext-spatial
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/okfn/ckanext-spatial.git#egg=ckanext-spatial
+RUN . /usr/lib/ckan/default/bin/activate && pip install -r /usr/lib/ckan/default/src/ckanext-spatial/pip-requirements.txt
+
+RUN . /usr/lib/ckan/default/bin/activate && pip install ckanext-geoview
+
+#restricted plugin
+#RUN . /usr/lib/ckan/default/bin/activate && pip install ckanext-restricted
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/CSIRO-enviro-informatics/ckanext-restricted.git#egg=ckanext-restricted
+
+
+#data requests
+RUN . /usr/lib/ckan/default/bin/activate && pip install ckanext-datarequests
+
+# Setup ckanext-org ckan Plugin
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e "git+https://github.com/datagovuk/ckanext-hierarchy.git#egg=ckanext-hierarchy"
+
+# Setup DAMC digital assets ckan Plugin
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/CSIRO-enviro-informatics/ckanext-digitalassetfields.git#egg=ckanext-digitalassetfields
+RUN . /usr/lib/ckan/default/bin/activate && pip install -r /usr/lib/ckan/default/src/ckanext-digitalassetfields/requirements.txt
+
+# Setup DAMC themes
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/CSIRO-enviro-informatics/ckanext-csiro_hub_theme.git#egg=ckanext-csiro_hub_theme
+
+# Setup User Extensions Plugin
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/CSIRO-enviro-informatics/ckanext-user_ext.git#egg=ckanext-user_ext
+RUN . /usr/lib/ckan/default/bin/activate && pip install -r /usr/lib/ckan/default/src/ckanext-user-ext/requirements.txt
+
+# Setup User Opt In Plugin (dependent on the User Extensions Plugin)
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/CSIRO-enviro-informatics/ckanext-user_opt_in.git#egg=ckanext-user_opt_in
+RUN . /usr/lib/ckan/default/bin/activate && pip install -r /usr/lib/ckan/default/src/ckanext-user-opt-in/requirements.txt
+
+# Setup EI's mod on dgu-organisation 
+RUN . /usr/lib/ckan/default/bin/activate && pip install -e git+https://github.com/CSIRO-enviro-informatics/ckanext-dgu_orgs.git#egg=ckanext-dgu_orgs
+#RUN . /usr/lib/ckan/default/bin/activate && pip install -r /usr/lib/ckan/default/src/ckanext-dgu_orgs/requirements.txt
+
+RUN apt-get install -y postgresql-client
+
+# Other ckan views
+RUN . /usr/lib/ckan/default/bin/activate && pip install ckanext-pdfview
+
+# SetUp EntryPoint
+COPY ./contrib/docker/wait-for-it.sh /
+RUN chmod +x /wait-for-it.sh
+RUN mkdir /entrypoint
+COPY ./contrib/docker/ckan-entrypoint.sh /entrypoint/
+RUN chmod +x /entrypoint/ckan-entrypoint.sh
+RUN ln -s /entrypoint/ckan-entrypoint.sh  /
+
+# Add Tini
+ENV TINI_VERSION v0.18.0
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
+RUN chmod +x /tini
+ENTRYPOINT ["/tini", "--", "/ckan-entrypoint.sh"]
+
+# Volumes
+VOLUME ["/entrypoint"]
+VOLUME ["/etc/ckan/default"]
+VOLUME ["/usr/lib/ckan"]
+VOLUME ["/var/lib/ckan"]
 EXPOSE 5000
-
-CMD ["ckan-paster","serve","/etc/ckan/production.ini"]
+CMD ["ckan-paster","serve","/etc/ckan/default/ckan.ini"]
--- contrib/docker/ckan-entrypoint.sh
+++ contrib/docker/ckan-entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 
 # URL for the primary database, in the format expected by sqlalchemy (required
@@ -8,10 +9,8 @@ set -e
 : ${CKAN_SOLR_URL:=}
 # URL for redis (required unless linked to a container called 'redis')
 : ${CKAN_REDIS_URL:=}
-# URL for datapusher (required unless linked to a container called 'datapusher')
-: ${CKAN_DATAPUSHER_URL:=}
 
-CONFIG="${CKAN_CONFIG}/production.ini"
+CONFIG="${CKAN_CONFIG}/ckan.ini"
 
 abort () {
   echo "$@" >&2
@@ -19,49 +18,268 @@ abort () {
 }
 
 set_environment () {
-  export CKAN_SITE_ID=${CKAN_SITE_ID}
-  export CKAN_SITE_URL=${CKAN_SITE_URL}
+  export CKAN_REMOTE_DEBUG_IP=${CKAN_REMOTE_DEBUG_IP}
   export CKAN_SQLALCHEMY_URL=${CKAN_SQLALCHEMY_URL}
   export CKAN_SOLR_URL=${CKAN_SOLR_URL}
   export CKAN_REDIS_URL=${CKAN_REDIS_URL}
-  export CKAN_STORAGE_PATH=/var/lib/ckan
-  export CKAN_DATAPUSHER_URL=${CKAN_DATAPUSHER_URL}
+  export CKAN_STORAGE_PATH=${CKAN_STORAGE_PATH}
+  export CKAN_SITE_URL=${CKAN_SITE_URL}
+  export CKAN_DEFAULT_ADMIN=${CKAN_SITE_URL}
+  export CKAN_HOME=${CKAN_HOME}
+  export CKAN_LDAP_PASSWORD=${CKAN_LDAP_PASSWORD}
+  export CKAN_ADMIN_PASSWORD=${CKAN_ADMIN_PASSWORD}
   export CKAN_DATASTORE_WRITE_URL=${CKAN_DATASTORE_WRITE_URL}
   export CKAN_DATASTORE_READ_URL=${CKAN_DATASTORE_READ_URL}
-  export CKAN_SMTP_SERVER=${CKAN_SMTP_SERVER}
-  export CKAN_SMTP_STARTTLS=${CKAN_SMTP_STARTTLS}
-  export CKAN_SMTP_USER=${CKAN_SMTP_USER}
-  export CKAN_SMTP_PASSWORD=${CKAN_SMTP_PASSWORD}
-  export CKAN_SMTP_MAIL_FROM=${CKAN_SMTP_MAIL_FROM}
-  export CKAN_MAX_UPLOAD_SIZE_MB=${CKAN_MAX_UPLOAD_SIZE_MB}
+  export MAPBOX_ACCESS_TOKEN=${MAPBOX_ACCESS_TOKEN}
+  export CKAN_DATAPUSHER_URL=${CKAN_DATAPUSHER_URL}
+  export BYPASS_DB_INIT=${BYPASS_DB_INIT}
 }
 
 write_config () {
+  echo "Writing Config"
+  printenv
+  echo "$CKAN_LDAP_PASSWORD"
+  # Note that this only gets called if there is no config, see below!
   ckan-paster make-config --no-interactive ckan "$CONFIG"
+
+  # The variables above will be used by CKAN, but
+  # in case want to use the config from ckan.ini use this
+  ckan-paster --plugin=ckan config-tool "$CONFIG" -e \
+      "sqlalchemy.url = ${CKAN_SQLALCHEMY_URL}" \
+      "solr_url = ${CKAN_SOLR_URL}" \
+      "ckan.redis.url = ${CKAN_REDIS_URL}" \
+      "ckan.storage_path = ${CKAN_STORAGE_PATH}" \
+      "ckan.site_url = ${CKAN_SITE_URL}"
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" -s DEFAULT "debug = false"
+  if [ "$DEBUG" == "1" ] || [ "$DEBUG" == "true" ]; then
+      ckan-paster --plugin=ckan config-tool "$CONFIG" -s DEFAULT "debug = true"
+  fi
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.uri = ldap://pool.ldap.csiro.au:389"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.base_dn = ou=People,dc=nexus,dc=csiro,dc=au"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.search.filter = uid={login}"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.email = mail"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.username = uid"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.auth.dn = sa-clw-ad-access@csiro.au"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.auth.password = ${CKAN_LDAP_PASSWORD}"
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.fullname = displayName"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.about = description"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.migrate = true"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.ckan_fallback = true"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.organization.id = csiro"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.ldap.organization.role = editor"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.cors.origin_allow_all = True"
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.spatial.srid = 4326"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.spatial.common_map.type = custom"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.spatial.search_backend = solr"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.spatial.common_map.custom.url =https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=${MAPBOX_ACCESS_TOKEN}"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.spatial.common_map.attribution = Map tiles by MapBox"
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckanext.geoview.ol_viewer.formats = kml geojson"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.views.default_views = image_view webpage_view recline_grid_view recline_map_view geo_view pdf_view geojson_view"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.activity_streams_email_notifications = False"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "email_to = oznomehelp@example.com"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "error_email_from = oznomehelp@csiro.au"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "smtp.mail_from = oznomehelp@csiro.au"
+#  ckan-paster --plugin=ckan config-tool "$CONFIG" "smtp.user = oznomehelp@csiro.au"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "smtp.server = smtp-relay.csiro.au"
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.datapusher.formats = csv xls xlsx tsv application/csv application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.datapusher.url = ${CKAN_DATAPUSHER_URL}"
+
+  if [ "$DEVENV" == "1" ] || [ "$DEVENV" == "true" ]; then
+      DEVPLUGIN="dev"
+  else
+      DEVPLUGIN=
+  fi
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.plugins = $DEVPLUGIN stats text_view image_view ldap datastore digitalassetfields csiro_hub_theme hierarchy_display hierarchy_form spatial_metadata spatial_query resource_proxy geo_view recline_grid_view recline_map_view recline_graph_view user_ext user_opt_in restricted datarequests dgu_orgs"
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.datarequests.comments = true"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.datarequests.show_datarequests_badge = false"
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.anon_create_dataset = false"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.create_unowned_dataset = true"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.create_dataset_if_not_in_organization = true"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.user_create_groups = true"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.user_delete_groups = true"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.user_create_organizations = false"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.user_delete_organizations = false"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.create_user_via_api = true"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.create_user_via_web = false"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.auth.roles_that_cascade_to_sub_groups = admin"
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.site_title = CSIRO Digital Asset Registry"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.site_logo = /images/csiro-lw-digital-asset-reg.png"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.favicon = /images/favicon.ico"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "licenses_group_url = http://staticcontent/licenses.json"
+
+
+  if [ "$DEVENV" == "1" ] || [ "$DEVENV" == "true" ]; then
+      ckan-paster --plugin=ckan config-tool "$CONFIG" "debug.remote = true"
+      ckan-paster --plugin=ckan config-tool "$CONFIG" "debug.remote.host.ip= ${CKAN_REMOTE_DEBUG_IP}"
+      ckan-paster --plugin=ckan config-tool "$CONFIG" "debug.remote.host.port = 6666"
+  fi
+
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "sqlalchemy.url = $(link_postgres_url)"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.datastore.write_url = $(link_datastore_postgres_url)"
+  ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.datastore.read_url = $(link_datastore_postgres_url)"
+
+  if [ "$PROD" == "1" ] || [ "$PROD" == "true" ]; then
+      ckan-paster --plugin=ckan config-tool "$CONFIG" -s "uwsgi" "socket = 0.0.0.0:3031" 
+      ckan-paster --plugin=ckan config-tool "$CONFIG" -s "uwsgi" "master = true" 
+      ckan-paster --plugin=ckan config-tool "$CONFIG" -s "uwsgi" "processes = 6" 
+      ckan-paster --plugin=ckan config-tool "$CONFIG" -s "uwsgi" "virtualenv = ${CKAN_HOME}"
+      ckan-paster --plugin=ckan config-tool "$CONFIG" -s "uwsgi" "threads = 2"
+      ckan-paster --plugin=ckan config-tool "$CONFIG" "ckan.cache_expires  = 3600"
+  fi
+}
+
+update_config () {
+  # The variables above will be used by CKAN, but
+  # in case want to use the config from ckan.ini use this
+  if [ "$DEBUG" == "1" ] || [ "$DEBUG" == "true" ]; then
+    ckan-paster --plugin=ckan config-tool "$CONFIG" -s DEFAULT "debug = true"
+  fi
+  ckan-paster --plugin=ckan config-tool "$CONFIG" -e \
+      "sqlalchemy.url = ${CKAN_SQLALCHEMY_URL}" \
+      "solr_url = ${CKAN_SOLR_URL}" \
+      "ckan.redis.url = ${CKAN_REDIS_URL}" \
+      "ckan.storage_path = ${CKAN_STORAGE_PATH}" \
+      "ckan.site_url = ${CKAN_SITE_URL}" \
+      "ckanext.ldap.auth.password = ${CKAN_LDAP_PASSWORD}"
+  if [ "$DEBUG" == "1" ] || [ "$DEBUG" == "true" ]; then
+    ckan-paster --plugin=ckan config-tool "$CONFIG" -e "debug.remote.host.ip = ${CKAN_REMOTE_DEBUG_IP}"
+  fi
+}
+
+link_datastore_postgres_url () {
+  local user=$DB_ENV_POSTGRES_USER
+  local pass=$DB_ENV_POSTGRES_PASSWORD
+  local db=datastore_default
+  local host=$DB_PORT_5432_TCP_ADDR
+  local port=$DB_PORT_5432_TCP_PORT
+  echo "postgresql://${user}:${pass}@${host}:${port}/${db}"
+}
+
+link_postgres_url () {
+  local user=$DB_ENV_POSTGRES_USER
+  local pass=$DB_ENV_POSTGRES_PASSWORD
+  local db=$DB_ENV_POSTGRES_DB
+  local host=$DB_PORT_5432_TCP_ADDR
+  local port=$DB_PORT_5432_TCP_PORT
+  echo "postgresql://${user}:${pass}@${host}:${port}/${db}"
+}
+
+link_solr_url () {
+  local host=$SOLR_PORT_8983_TCP_ADDR
+  local port=$SOLR_PORT_8983_TCP_PORT
+  echo "http://${host}:${port}/solr/ckan"
+}
+
+link_redis_url () {
+  local host=$REDIS_PORT_6379_TCP_ADDR
+  local port=$REDIS_PORT_6379_TCP_PORT
+  echo "redis://${host}:${port}/1"
 }
 
 # If we don't already have a config file, bootstrap
 if [ ! -e "$CONFIG" ]; then
   write_config
+else
+  update_config
 fi
 
-# Get or create CKAN_SQLALCHEMY_URL
+# Set environment variables
 if [ -z "$CKAN_SQLALCHEMY_URL" ]; then
-  abort "ERROR: no CKAN_SQLALCHEMY_URL specified in docker-compose.yml"
+  if ! CKAN_SQLALCHEMY_URL=$(link_postgres_url); then
+    abort "ERROR: no CKAN_SQLALCHEMY_URL specified and linked container called 'db' was not found"
+  fi
 fi
 
 if [ -z "$CKAN_SOLR_URL" ]; then
-    abort "ERROR: no CKAN_SOLR_URL specified in docker-compose.yml"
+  if ! CKAN_SOLR_URL=$(link_solr_url); then
+    abort "ERROR: no CKAN_SOLR_URL specified and linked container called 'solr' was not found"
+  fi
 fi
 
 if [ -z "$CKAN_REDIS_URL" ]; then
-    abort "ERROR: no CKAN_REDIS_URL specified in docker-compose.yml"
-fi
-
-if [ -z "$CKAN_DATAPUSHER_URL" ]; then
-    abort "ERROR: no CKAN_DATAPUSHER_URL specified in docker-compose.yml"
+  if ! CKAN_REDIS_URL=$(link_redis_url); then
+    abort "ERROR: no CKAN_REDIS_URL specified and linked container called 'redis' was not found"
+  fi
 fi
 
 set_environment
-ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
+
+create_org_and_user_membership () {
+  set +e
+  export PGPASSWORD=$DB_ENV_POSTGRES_PASSWORD
+  cat <<EOF | psql -h $DB_PORT_5432_TCP_ADDR -p $DB_PORT_5432_TCP_PORT -U $DB_ENV_POSTGRES_USER --set ON_ERROR_STOP=1 || true
+INSERT INTO ckan.public.revision (id, timestamp, author, message, state)
+VALUES ('1e662e20-0c88-492d-8deb-7cac39ae6a14', (now() at time zone 'utc'), 'admin', 'Create CSIRO group and add admin user as member.', 'active')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO ckan.public.group (id, name, title, description, created, state, revision_id, type, is_organization)
+VALUES ('e70f6776-05f3-48a7-9c24-2fff14e7899a', 'csiro', 'CSIRO', 'CSIRO', (now() at time zone 'utc'), 'active', '1e662e20-0c88-492d-8deb-7cac39ae6a14', 'organization', TRUE)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO ckan.public.group_revision (id, name, title, description, created, state, revision_id, continuity_id, revision_timestamp, type, is_organization)
+VALUES ('e70f6776-05f3-48a7-9c24-2fff14e7899a', 'csiro', 'CSIRO', 'CSIRO', (now() at time zone 'utc'), 'active', '1e662e20-0c88-492d-8deb-7cac39ae6a14', 'e70f6776-05f3-48a7-9c24-2fff14e7899a', (now() at time zone 'utc'), 'organization', TRUE)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO ckan.public.member (id, table_id, group_id, state, revision_id, table_name, capacity)
+VALUES ('e5c180c3-7bbf-4e9a-b060-e14a8d2e58f7', (SELECT id FROM ckan.public.user WHERE name LIKE 'admin' LIMIT 1), 'e70f6776-05f3-48a7-9c24-2fff14e7899a', 'active', '1e662e20-0c88-492d-8deb-7cac39ae6a14', 'user', 'admin')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO ckan.public.member_revision (id, table_id, group_id, state, revision_id, continuity_id, revision_timestamp, table_name, capacity)
+VALUES ('e5c180c3-7bbf-4e9a-b060-e14a8d2e58f7', (SELECT id FROM ckan.public.user WHERE name LIKE 'admin' LIMIT 1), 'e70f6776-05f3-48a7-9c24-2fff14e7899a', 'active', '1e662e20-0c88-492d-8deb-7cac39ae6a14', 'e5c180c3-7bbf-4e9a-b060-e14a8d2e58f7', (now() at time zone 'utc'), 'user', 'admin')
+ON CONFLICT DO NOTHING;
+EOF
+  unset PGPASSWORD
+  set -e
+}
+
+create_local_org () {
+  set +e
+  export PGPASSWORD=$DB_ENV_POSTGRES_PASSWORD
+  cat <<EOF | psql -h $DB_PORT_5432_TCP_ADDR -p $DB_PORT_5432_TCP_PORT -U $DB_ENV_POSTGRES_USER --set ON_ERROR_STOP=1 || true
+INSERT INTO ckan.public.revision (id, timestamp, author, message, state)
+VALUES ('e03c5eab-60db-4524-9245-61efb5045390', (now() at time zone 'utc'), 'admin', 'Create local group and add admin user as member.', 'active')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO ckan.public.group (id, name, title, description, created, state, revision_id, type, is_organization)
+VALUES ('6d9337de-8140-4911-9a3f-8c02cdc166fa', 'local', 'Local Assets', 'Local L&W created assets', (now() at time zone 'utc'), 'active', 'e03c5eab-60db-4524-9245-61efb5045390', 'group', FALSE)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO ckan.public.group_revision (id, name, title, description, created, state, revision_id, continuity_id, revision_timestamp, type, is_organization)
+VALUES ('6d9337de-8140-4911-9a3f-8c02cdc166fa', 'local', 'Local Assets', 'Local L&W created assets', (now() at time zone 'utc'), 'active', 'e03c5eab-60db-4524-9245-61efb5045390', '6d9337de-8140-4911-9a3f-8c02cdc166fa', (now() at time zone 'utc'), 'group', FALSE)
+ON CONFLICT DO NOTHING;
+
+EOF
+  unset PGPASSWORD
+  set -e
+}
+
+#. ./wait-for-it.sh -t 0 -h http://solr -p 8983 -- echo "solr is up"
+
+./wait-for-it.sh -t 0 -h solr -p 8983 -- echo "solr is up"
+./wait-for-it.sh -t 0 -h db -p 5432 -- echo "db is up"
+
+if [ -z "$BYPASS_DB_INIT" ]; then
+	echo "Initializing plugins and database"
+	# Initializes the Database
+	ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/ckan.ini"
+	ckan-paster --plugin=ckanext-spatial spatial initdb -c "${CKAN_CONFIG}/ckan.ini"
+	set +e
+	export PGPASSWORD=$DB_ENV_POSTGRES_PASSWORD
+	ckan-paster --plugin=ckan datastore set-permissions  -c "${CKAN_CONFIG}/ckan.ini"  | psql -h $DB_PORT_5432_TCP_ADDR -p $DB_PORT_5432_TCP_PORT -U $DB_ENV_POSTGRES_USER --set ON_ERROR_STOP=1 || true
+	unset PGPASSWORD
+	set -e
+fi
+ckan-paster --plugin=ckan user add admin password=${CKAN_ADMIN_PASSWORD} email=OznomeHelp@csiro.au -c "${CKAN_CONFIG}/ckan.ini" || true
+create_org_and_user_membership
+create_local_org
+ckan-paster --plugin=ckan sysadmin add admin -c "${CKAN_CONFIG}/ckan.ini" || true
 exec "$@"
